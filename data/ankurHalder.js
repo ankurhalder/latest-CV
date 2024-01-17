a51473cedf8004729a2b5beb11ec7ee348d211bf
Ankur Halder <ankur.halder12345@gmail.com>
2024-01-17 18:28:47 +0530

    ankurHalder.js created

---
+++ data/ankurHalder.js
@@ -0,0 +1,196 @@
+const ankurHalder = {
+  personalInfo: {
+    name: "Ankur Halder",
+    email: "ankur.halder12345@gmail.com",
+    phone: "+91 9748903490",
+    location: "Kolkata",
+    image: "/Ankur_halder_12021002022073_PASSPORT.jpg",
+  },
+  education: [
+    {
+      degree: "B.Tech, Computer Science",
+      institution: "University Of Engineering & Management, New Town",
+      duration: "2021 - 2025",
+      image: "/uem.png",
+    },
+    {
+      degree: "Senior Secondary (XII), Science",
+      institution: "SHRI RITAM VIDYAPITH (WBCHSE board)",
+      duration: "Year of completion: 2021",
+      percentage: "Percentage: 76.60%",
+      image: "/ritam.png",
+    },
+    {
+      degree: "Secondary (X)",
+      institution: "Behala Aryya Vidyamandir (WBBSE board)",
+      duration: "Year of completion: 2017",
+      percentage: "Percentage: 85.70%",
+      image: "/bav.png",
+    },
+  ],
+  workExperience: {
+    job: "None",
+    internship: {
+      title: "Student Intern",
+      company: "CODSOFT",
+      description:
+        "Congratulations! You've successfully completed the Student Internship Program : CIP. On behalf of CODSOFT we are pleased to issue your CIP Certificate.",
+      duration: "10/10/2023 - 10/11/2023",
+      type: "Virtual/Work from Home",
+      topic: "Web Development",
+      certification: {
+        url: "https://drive.google.com/file/d/1UTuhpUyL-AEO9VyOQgXhRHm-IyR-CVvb/view",
+        credentialID: "4a5af54",
+      },
+      image: "/codsoft.png",
+    },
+  },
+  positionsOfResponsibility: [
+    {
+      position: "College Project Leader",
+      description:
+        "Leading a 6-member team in the ongoing 'Universa' college group project. Organizing tasks, ensuring progress, and achieving project milestones.",
+    },
+  ],
+  trainingsCourses: [
+    {
+      title: "Next.js & React - The Complete Guide (incl. Two Paths!)",
+      institution: "udemy, Online",
+      duration: "Jul 2023 - Sep 2023",
+      certificate: {
+        url: "https://www.udemy.com/certificate/UC-fc0143a4-6b19-4e07-bcee-53f7c1f9fd1d/",
+      },
+    },
+    {
+      title: "The Ultimate React Course 2023: React, Redux & More",
+      institution: "udemy, Online",
+      duration: "Mar 2023 - Jul 2023",
+      certificate: {
+        url: "https://www.udemy.com/certificate/UC-3b120d82-005f-43b9-a60e-2fe949bb1f8c/",
+      },
+    },
+    {
+      title: "Meta Front-End Developer (Professional Certificate)",
+      institution: "Coursera, Online",
+      duration: "Feb 2023 - Aug 2023",
+      certificate: {
+        url: "https://www.coursera.org/account/accomplishments/specialization/certificate/4GVM3WFXRHB6/",
+      },
+      credentialID: "4GVM3WFXRHB6",
+      skills: [
+        "Introduction to Front-End Development",
+        "HTML and CSS in Depth",
+        "Programming with JavaScript",
+        "React Basics",
+        "Advanced React",
+        "Coding Interview Preparation",
+        "Version Control",
+        "Principles of UX/UI Design",
+        "Front-End Developer Capstone",
+      ],
+    },
+    {
+      title: "Node.js, Express, MongoDB & More: The Complete Bootcamp",
+      institution: "udemy, Online",
+      duration: "Jan 2023 - Sep 2023",
+      certificate: {
+        url: "https://www.udemy.com/certificate/UC-ea6816f6-5c07-4069-870d-65486f34d3ef/",
+      },
+    },
+    {
+      title: "Explore A Career In Front-End Web Development",
+      institution: "LinkedIn, Online",
+      duration: "Dec 2022 - Aug 2023",
+      credentials: {
+        url: "https://www.linkedin.com/learning/certificates/26a8bcdc2bbda4c723aa1c3b18d6bdfdc163050f32c3a7f62d7fef380be07d4d?u=229219690",
+      },
+      description:
+        "I have completed a comprehensive learning path in front-end web development, complemented by a certification. This path has equipped me with the essential skills to design and develop engaging, interactive user experiences on the web, including proficiency in HTML, CSS, JavaScript, and React. Furthermore, I have gained knowledge in accessibility techniques and a deep understanding of web concepts, enabling me to create inclusive and user-friendly web applications.",
+    },
+    {
+      title: "The Complete JavaScript Course 2023: From Zero To Expert!",
+      institution: "udemy, Online",
+      duration: "Aug 2022 - Jan 2023",
+      certificate: {
+        url: "https://www.udemy.com/certificate/UC-19c4ac79-892a-47ad-b473-06600f7300c8/",
+      },
+    },
+  ],
+  skills: [
+    "Python - Intermediate",
+    "JavaScript - Advanced",
+    "HTML - Advanced",
+    "CSS - Intermediate",
+    "SCSS - Intermediate",
+    "React - Advanced",
+    "Next.js - Advanced",
+    "Node.js - Intermediate",
+    "Express.js - Intermediate",
+    "mongoose - Intermediate",
+    "MongoDB - Intermediate",
+    "Git - Intermediate",
+    "GitHub - Intermediate",
+    "C Programming - Intermediate",
+    "Data Structures - Intermediate",
+  ],
+  portfolio: [
+    {
+      title: "Adcomsys Conference Website",
+      description:
+        "Co-created Adcomsys conference website in collaboration with a classmate. Built using Next.js, React.js, and SCSS. Scheduled for 2024. For more information, please type 'who created this website?' in the chatbot of the official website.",
+      link: "https://adcomsys.uem.edu.in",
+      image: "/adcomsys.png",
+    },
+    {
+      title: "Space Otaku",
+      description:
+        "Space Otaku: Explore space news, astronaut updates, satellite tracking, and more! Powered by Next.js, Sass, and Dialogflow for an immersive, interactive experience. Stay informed about the cosmos.",
+      link: "https://www.spaceotaku.online",
+      image: "/spaceotaku.png",
+    },
+    {
+      title: "Eduversa",
+      description:
+        "As the project leader, I spearheaded the development of Universa URP System, enhancing academic processes, and showcasing my exceptional leadership and project management skills, culminating in a successful college group project.",
+      link: "http://eduversa.in",
+      image: "/eduversa.png",
+    },
+  ],
+  socialProfiles: {
+    linkedIn: {
+      link: "https://www.linkedin.com/in/ankurhalder",
+      image: "linkedin.png",
+    },
+    github: {
+      link: "https://github.com/ankurhalder",
+      image: "github.png",
+    },
+    leetcode: {
+      link: "https://leetcode.com/ankurhalder",
+      image: "leetcode.png",
+    },
+
+    facebook: {
+      link: "https://www.facebook.com/ankur.roxx.9",
+      image: "facebook.png",
+    },
+    instagram: {
+      link: "https://www.instagram.com/halder_ankur",
+      image: "instagram.png",
+    },
+    geekforgeeks: {
+      link: "https://auth.geeksforgeeks.org/user/ankurhalder",
+      image: "geekforgeeks.png",
+    },
+    codechef: {
+      link: "https://www.codechef.com/users/ankurhalder123",
+      image: "codechef.png",
+    },
+    hackerrank: {
+      link: "https://www.hackerrank.com/profile/ankurhalder",
+      image: "hackerrank.png",
+    },
+  },
+};
+
+export default ankurHalder;
